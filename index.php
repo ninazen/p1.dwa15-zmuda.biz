3a67f71df3c3c655d0aca95c8c7e91181434b395
Nina Zmuda <ninazephyr@yahoo.com>
2013-09-17 01:08:22 -0700

    Added comments

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <html>
 
 <head>
+	<!--Project 1 - Workflow setup and HTML/CSS-->
 	<title>CSCI E-15 P1</title>
+
+	<!--CSS styles-->
 	<style>
 		h1 {font-size:150%;}
 		h2 {font-size:180%;}
@@ -12,15 +15,18 @@
 
 <body>
 	<center>
-	<br/><br/><br/><br/><br/>
+	<br><br><br><br><br>
 	<h1><p>Welcome to P1.</p></h1>
 
+		<!--Name-->
 		<h2><p>My name is Nina Zmuda.</p></h2>
 
-			<h3><p>I am an extension student in CSCI E-15.<br/>
-			I currently live on the West Coast, by the ocean.<br/>
+			<!--Short Bio-->
+			<h3><p>I am an extension student in CSCI E-15.<br>
+			I currently live on the West Coast, by the ocean.<br>
 			I enjoy cooking, running and surfing the web.</p></h3>
 
+		<!--Text Editor-->
 		<h2><p>This was done in the Sublime Text editor.</p></h2>
  
 	<h1><p>Thank you for visiting.</p></h1>
