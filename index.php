49533d3f6c7c35148b32373163917be5380cdf75
Nina Zmuda <ninazephyr@yahoo.com>
2013-09-16 23:59:12 -0700

    Adding profile information

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-Welcome to P1 
+<html>
+ <head>
+  <title>CSCI E-15 P1</title>
+ </head>
+ <body>
+ <center>
+ <br/><br/><br/><br/><br/>
+ <p>Welcome to P1.</p>
+
+ <p>My name is Nina Zmuda.
+
+ <p>I am an extension student in CSCI E-15.<br/>
+ I currently live on the West Coast, by the ocean.<br/>
+ I enjoy cooking, running and surfing the web.</p>
+
+ <p>This was written using the Sublime Text editor.</p>
+ <p>Thank you for visiting.</p>
+ </center>
+ </body>
+</html>
